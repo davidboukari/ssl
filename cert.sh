4478ac640c54e8b3d6371cd32b40c98897654aaa
David Boukari <davidboukari@gmail.com>
2021-04-12 08:00:36 +0200

    Same domains

--- cert.sh
+++ cert.sh
@@ -1,75 +1,70 @@
 #!/bin/bash -x
 
-CA=ca
-CERTNAME=logstash.myelk.com
+CA_SRV=ca_srv
+CA_CLIENT=ca_client
+CERTNAME_SRV=logstash.myelk.com
 CERTNAME_CLIENT=filebeat.myelk.com
-DESTDIR=cert
+DESTDIR_SRV=cert_srv
+DESTDIR_CLIENT=cert_client
 SERIAL=ca.srl
 
-rm -fr $DESTDIR
-mkdir -p $DESTDIR
+rm -fr $DESTDIR_SRV $DESTDIR_CLIENT
+mkdir -p $DESTDIR_SRV $DESTDIR_CLIENT
 
 # Create the private key, add -des3 for passphrase
-openssl genrsa  -out ${DESTDIR}/${CERTNAME}.key  1024
+openssl genrsa  -out ${DESTDIR_SRV}/${CERTNAME_SRV}.key  1024
 
 # Create the CSR Certificate Signing Request for the CRT
-openssl req   -new -key ${DESTDIR}/${CERTNAME}.key -out ${DESTDIR}/${CERTNAME}.csr  -config ${CERTNAME}.conf -subj "/C=FR/ST=France/L=Paris/O=MyElk/CN=logstash.myelk.com"
-#openssl req -new -key ${DESTDIR}/${CERTNAME}.key -out ${DESTDIR}/${CERTNAME}.csr  -config ${CERTNAME}.conf<<EOF
-#FR
-#France
-#Paris
-#MyElk
-#myelk.com
-#EOF
-#exit 100
+openssl req   -new -key ${DESTDIR_SRV}/${CERTNAME_SRV}.key -out ${DESTDIR_SRV}/${CERTNAME_SRV}.csr  -config ${CERTNAME_SRV}.conf -subj "/C=FR/ST=France/L=Paris/O=MyElk/CN=logstash.myelk.com"
+
 # Create the CA Certification Authority key
-openssl genrsa -out ${DESTDIR}/ca.key 1024
-#openssl x509 -in ${DESTDIR}/ca.crt -text -noout -serial|grep serial|cut -d'=' -f2 > ${DESTDIR}/${SERIAL}
+openssl genrsa -out ${DESTDIR_SRV}/ca.key 1024
+#openssl x509 -in ${DESTDIR_SRV}/ca.crt -text -noout -serial|grep serial|cut -d'=' -f2 > ${DESTDIR_SRV}/${SERIAL}
 
 # Create the CA auto signed certificate CRT
-openssl req -new -x509 -days 365 -key ${DESTDIR}/ca.key  -out ${DESTDIR}/ca.crt  -config ${CA}.conf -subj "/C=FR/ST=France/L=Paris/O=MyElk/CN=myelk.com"
-#openssl req -new -x509 -days 365 -key ${DESTDIR}/ca.key  -out ${DESTDIR}/ca.crt  -config ${CERTNAME}.conf<<EOF
-#FR
-#France
-#Paris
-#MyElk
-#myelk.com
-#EOF
+openssl req -new -x509 -days 365 -key ${DESTDIR_SRV}/ca.key  -out ${DESTDIR_SRV}/ca.crt  -config ${CA_SRV}.conf -subj "/C=FR/ST=France/L=Paris/O=MyElk/CN=myelk.com"
 
 # Sign the client CRT
-#The option -CAcreateserial should be use only the 1st time, it will generate ca.srl.
-#For the next certifications (to renew or others domaines) the ID in ca.srl will be incremented with the option -CAserial ca.srl
-#if [ ! -f ${SERIAL} ];then
-#  openssl x509 -req -in ${DESTDIR}/${CERTNAME}.csr -out ${DESTDIR}/${CERTNAME}.crt -CA ${DESTDIR}/ca.crt -CAkey ${DESTDIR}/ca.key -CAcreateserial -CAserial ${DESTDIR}/${SERIAL} -extensions v3_req -extfile ${CERTNAME}.conf 
-#else
-#  openssl x509 -req -in ${DESTDIR}/${CERTNAME}.csr -out ${DESTDIR}/${CERTNAME}.crt -CA ${DESTDIR}/ca.crt -CAkey ${DESTDIR}/ca.key  -CAserial ${DESTDIR}/${SERIAL} -extensions v3_req -extfile ${CERTNAME}.conf
-#fi
-openssl x509 -req -in ${DESTDIR}/${CERTNAME}.csr -out ${DESTDIR}/${CERTNAME}.crt -CA ${DESTDIR}/ca.crt -CAkey ${DESTDIR}/ca.key  -CAcreateserial   -CAserial ${DESTDIR}/${SERIAL} -extensions v3_req -extfile ${CERTNAME}.conf
+openssl x509 -req -in ${DESTDIR_SRV}/${CERTNAME_SRV}.csr -out ${DESTDIR_SRV}/${CERTNAME_SRV}.crt -CA ${DESTDIR_SRV}/ca.crt -CAkey ${DESTDIR_SRV}/ca.key  -CAcreateserial   -CAserial ${DESTDIR_SRV}/${SERIAL} -extensions v3_req -extfile ${CERTNAME_SRV}.conf
+
+
 
 # Create the private key, add -des3 for passphrase
-openssl genrsa 1024 > ${DESTDIR}/${CERTNAME_CLIENT}.key 
+openssl genrsa 1024 > ${DESTDIR_CLIENT}/${CERTNAME_CLIENT}.key 
 
 # Create the CSR Certificate Signing Request for the CRT
-openssl req -new -key ${DESTDIR}/${CERTNAME_CLIENT}.key -out ${DESTDIR}/${CERTNAME_CLIENT}.csr  -subj "/C=FR/ST=France/L=Paris/O=MyElk/CN=filebeat.myelk.com"
+openssl req -new -key ${DESTDIR_CLIENT}/${CERTNAME_CLIENT}.key -out ${DESTDIR_CLIENT}/${CERTNAME_CLIENT}.csr  -subj "/C=FR/ST=France/L=Paris/O=MyElk/CN=filebeat.myelk.com"
 
-openssl x509 -req -in ${DESTDIR}/${CERTNAME_CLIENT}.csr -out ${DESTDIR}/${CERTNAME_CLIENT}.crt -CA ${DESTDIR}/ca.crt -CAkey ${DESTDIR}/ca.key  -CAserial ${DESTDIR}/${SERIAL} -extensions v3_req -extensions usr_cert -extfile ${CERTNAME_CLIENT}.conf
+# Create the CA Certification Authority key
+#openssl genrsa -out ${DESTDIR_CLIENT}/ca.key 1024
+
+# Create the CA auto signed certificate CRT
+#openssl req -new -x509 -days 365 -key ${DESTDIR_CLIENT}/ca.key  -out ${DESTDIR_CLIENT}/ca.crt  -config ${CA_CLIENT}.conf -subj "/C=FR/ST=France/L=Paris/O=MyElk/CN=myelk.com"
+
+cp ${DESTDIR_SRV}/ca.* ${DESTDIR_CLIENT}
+
+# Create the CRT
+#openssl x509 -req -in ${DESTDIR_CLIENT}/${CERTNAME_CLIENT}.csr -out ${DESTDIR_CLIENT}/${CERTNAME_CLIENT}.crt -CA ${DESTDIR_CLIENT}/ca.crt -CAkey ${DESTDIR_CLIENT}/ca.key -CAcreateserial  -CAserial ${DESTDIR_CLIENT}/${SERIAL} -extensions v3_req -extensions usr_cert -extfile ${CERTNAME_CLIENT}.conf
+openssl x509 -req -in ${DESTDIR_CLIENT}/${CERTNAME_CLIENT}.csr -out ${DESTDIR_CLIENT}/${CERTNAME_CLIENT}.crt -CA ${DESTDIR_CLIENT}/ca.crt -CAkey ${DESTDIR_CLIENT}/ca.key  -CAserial ${DESTDIR_CLIENT}/${SERIAL} -extensions v3_req -extensions usr_cert -extfile ${CERTNAME_CLIENT}.conf
 
 # Convert key to pkcs8
-openssl pkcs8 -in ${DESTDIR}/${CERTNAME}.key -topk8 -nocrypt -out ${DESTDIR}/${CERTNAME}.pkcs8.key
-openssl pkcs8 -in ${DESTDIR}/${CERTNAME_CLIENT}.key -topk8 -nocrypt -out ${DESTDIR}/${CERTNAME_CLIENT}.pkcs8.key
+openssl pkcs8 -in ${DESTDIR_SRV}/${CERTNAME_SRV}.key -topk8 -nocrypt -out ${DESTDIR_SRV}/${CERTNAME_SRV}.pkcs8.key
+openssl pkcs8 -in ${DESTDIR_CLIENT}/${CERTNAME_CLIENT}.key -topk8 -nocrypt -out ${DESTDIR_CLIENT}/${CERTNAME_CLIENT}.pkcs8.key
 
 # Verify the CRT
-openssl verify  -CAfile ${DESTDIR}/ca.crt  ${DESTDIR}/${CERTNAME}.crt
+openssl verify  -CAfile ${DESTDIR_SRV}/ca.crt  ${DESTDIR_SRV}/${CERTNAME_SRV}.crt
 read
-openssl verify  -CAfile ${DESTDIR}/ca.crt  ${DESTDIR}/${CERTNAME_CLIENT}.crt
+openssl verify  -CAfile ${DESTDIR_CLIENT}/ca.crt  ${DESTDIR_CLIENT}/${CERTNAME_CLIENT}.crt
 read
 
 # Read the CRT
-openssl x509 -text -serial  -noout -in ${DESTDIR}/ca.crt
+openssl x509 -text -serial  -noout -in ${DESTDIR_SRV}/ca.crt
 read
-openssl x509 -text -serial -noout -in ${DESTDIR}/${CERTNAME}.crt
+openssl x509 -text -serial -noout -in ${DESTDIR_SRV}/${CERTNAME_SRV}.crt
 read 
-openssl x509 -text -serial -noout -in ${DESTDIR}/${CERTNAME_CLIENT}.crt
+openssl x509 -text -serial  -noout -in ${DESTDIR_CLIENT}/ca.crt
+read
+openssl x509 -text -serial -noout -in ${DESTDIR_CLIENT}/${CERTNAME_CLIENT}.crt
 
 
 
